6d7ae8a48cde57f8972c2e5cc8ee6b1bd99a6855
Setayesh <75073451+setykarimi@users.noreply.github.com>
2022-11-14 12:14:38 +0330

    add sidebar,navbar,feed,rightbar components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
+
+import { Stack } from '@mui/material';
 import './App.css';
+import Feed from './components/Feed';
+import Navabar from './components/Navbar';
+import Rightbar from './components/Rightbar';
+import Sidebar from './components/Sidebar';
 
 function App() {
 
   return (
     <div className="App">
-    Hello
+      <Navabar />
+      <Stack direction="row" spacing={2} justifyContent="space-between">
+      <Sidebar />
+      <Feed />
+      <Rightbar />
+      </Stack>
     </div>
   );
 }
