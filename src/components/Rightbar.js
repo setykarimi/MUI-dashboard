1315e2382dfa024f89d86f9c87f42400c76fb04f
Setayesh <75073451+setykarimi@users.noreply.github.com>
2022-11-15 11:15:28 +0330

    Add rightbar title

--- src/components/Rightbar.js
+++ src/components/Rightbar.js
@@ -1,8 +1,13 @@
-import { Box } from "@mui/material"
+import { Box, Typography } from "@mui/material"
 
 const Rightbar = () => {
-    return( 
-        <Box flex={2} p={2} sx={{dispaly: {xs: "none" , sm: "block"}}}>Rightbar</Box>
+    return (
+        <Box flex={1} p={2} sx={{ display: { xs: "none", md: "block" } }}>
+            <Box position="fixed">
+                <Typography variant="h6" fontWeight={400}>Online Friends</Typography>
+            </Box>
+        </Box>
+
     )
 }
 
