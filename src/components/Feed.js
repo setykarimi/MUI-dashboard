c042c0909cba7efbf21c3d58fe4e7601fcbce2e3
Setayesh <75073451+setykarimi@users.noreply.github.com>
2022-11-15 11:12:47 +0330

    Add grid display, Post compoent

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -1,8 +1,17 @@
-import { Box } from "@mui/material"
+import { Box, Grid } from "@mui/material"
+import Post from "./Post"
 
 const Feed = () => {
-    return( 
-        <Box flex={4} p={2}>Feed</Box>
+    return (
+        <Box flex={4} p={2}>
+            <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+                <Post />
+                <Post />
+                <Post />
+                <Post />
+                <Post />
+            </Grid>
+        </Box>
     )
 }
 
