373f113a7c121fb0ec030835c6668b2f38ec4f8a
Setayesh <75073451+setykarimi@users.noreply.github.com>
2022-11-15 11:12:16 +0330

    Create new component for posts in Feed component

---
+++ src/components/Post.jsx
@@ -0,0 +1,48 @@
+import {  Favorite, FavoriteBorder, MoreVert, Share } from "@mui/icons-material"
+import { Avatar,  Card, CardActions, CardContent, CardHeader, CardMedia, IconButton, Typography, Checkbox, Grid } from "@mui/material";
+
+const Post = () => {
+    return (
+        <Grid item lg={6} sm={12}>
+        <Card>
+        <CardHeader
+            avatar={
+                <Avatar sx={{ bgcolor: "red" }} aria-label="recipe">
+                    R
+                </Avatar>
+            }
+            action={
+                <IconButton aria-label="settings">
+                    <MoreVert />
+                </IconButton>
+            }
+            title="Shrimp and Chorizo Paella"
+            subheader="September 14, 2016"
+        />
+        <CardMedia
+            component="img"
+            height="250"
+            image="https://cleanfoodcrush.com/wp-content/uploads/2020/10/Clean-Food-Crush-Spicy-New-Orleans-Inspired-Shrimp-Recipe.jpg"
+            alt="Paella dish"
+        />
+        <CardContent>
+            <Typography variant="body2" color="text.secondary">
+                This impressive paella is a perfect party dish and a fun meal to cook
+                together with your guests. Add 1 cup of frozen peas along with the mussels,
+                if you like.
+            </Typography>
+        </CardContent>
+        <CardActions disableSpacing>
+            <IconButton aria-label="add to favorites">
+            <Checkbox icon={<FavoriteBorder />} checkedIcon={<Favorite sx={{ color: "red" }}/>} />
+            </IconButton>
+            <IconButton aria-label="share">
+                <Share />
+            </IconButton>
+        </CardActions>
+    </Card>
+    </Grid>
+    )
+}
+
+export default Post 
