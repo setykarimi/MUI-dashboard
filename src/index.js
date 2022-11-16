07b71601b095915924dbc4ae5a4d733248e6f49a
Setayesh <75073451+setykarimi@users.noreply.github.com>
2022-11-16 12:12:54 +0330

    remove ThemeProvider from index.js

--- src/index.js
+++ src/index.js
@@ -1,26 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { ThemeProvider, createTheme } from '@material-ui/core';
-
+import './index.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-const theme = createTheme({
-  palette: {
-    primary: {
-      main: "#CCC"
-    }
-  },
-  typography: {
-    myVariant: {
-      color: "#fefefe",
-      fontSize: "6rem"
-    }
-  }
-})
+
 root.render(
-<ThemeProvider theme={theme}>
     <App />
-    </ThemeProvider>
 );
 
