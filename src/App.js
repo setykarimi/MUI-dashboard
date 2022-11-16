0f8531eaadd3cf250cd6618f4b43bfb249a53cc4
Setayesh <75073451+setykarimi@users.noreply.github.com>
2022-11-16 12:12:35 +0330

    Add Theme Provider and mode const for changing mode

--- src/App.js
+++ src/App.js
@@ -1,22 +1,34 @@
 
-import { Stack } from '@mui/material';
+import { createTheme, Stack, ThemeProvider } from '@mui/material';
+import { Box } from '@mui/system';
+import { useState } from 'react';
 import './App.css';
+import Add from './components/Add';
 import Feed from './components/Feed';
 import Navabar from './components/Navbar';
 import Rightbar from './components/Rightbar';
 import Sidebar from './components/Sidebar';
 
 function App() {
+  const [mode, setMode] = useState("light")
+  const darkTheme = createTheme({
+    palette: {
+      mode: mode
+    }
+  })
 
   return (
-    <div className="App">
-      <Navabar />
-      <Stack direction="row" spacing={2} justifyContent="space-between">
-      <Sidebar />
-      <Feed />
-      <Rightbar />
-      </Stack>
-    </div>
+    <ThemeProvider theme={darkTheme}>
+      <Box bgcolor={"background.default"} color={"text.primary"}>
+        <Navabar />
+        <Stack direction="row" spacing={2} justifyContent="space-between">
+          <Sidebar setMode={setMode} mode={mode}/>
+          <Feed />
+          <Rightbar />
+          <Add />
+        </Stack>
+      </Box>
+    </ThemeProvider>
   );
 }
 
