22a455bf191019ca35c1a76be519a9b409bef29a
Setayesh <75073451+setykarimi@users.noreply.github.com>
2022-11-27 20:53:06 +0330

    Add color, name, title, subheader for posts in Feed compoent

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -5,11 +5,11 @@ const Feed = () => {
     return (
         <Box flex={4} p={2}>
             <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
-                <Post />
-                <Post />
-                <Post />
-                <Post />
-                <Post />
+                <Post color="cyan" name="R" title="Pasta" subheader="September 14, 2016"/>
+                <Post color="blue" name="B" title="Pizza" subheader="Octber 21, 2022"/>
+                <Post color="purple" name="S" title="Salad" subheader="November 22, 2021"/>
+                <Post color="pink" name="T" title="Kebab" subheader="December 23, 2018"/>
+                <Post color="orange" name="LA" title="Tiramisu" subheader="July 25, 2021"/>
             </Grid>
         </Box>
     )
